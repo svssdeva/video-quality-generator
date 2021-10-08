50f829559811f36ed20a582cebfc3d76fb853357
Nitish Mishra <mnitish287@gmail.com>
2021-10-08 15:06:22 +0530

    yt player setup done

--- android/src/main/java/deva/youtube/generator/DevaYoutubeGenerator.java
+++ android/src/main/java/deva/youtube/generator/DevaYoutubeGenerator.java
@@ -3,6 +3,7 @@ package deva.youtube.generator;
 import android.content.Context;
 import android.util.Log;
 import android.util.SparseArray;
+import android.widget.Toast;
 
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
@@ -12,12 +13,28 @@ import com.getcapacitor.PluginMethod;
 import com.google.gson.Gson;
 import com.pw.nativeplayer.NativePlayerModal;
 import com.pw.nativeplayer.modals.BatchCredential;
+import com.pw.nativeplayer.response.BatchData;
+import com.pw.nativeplayer.response.CourseContentData;
+import com.pw.nativeplayer.response.DemoVideosData;
+import com.pw.nativeplayer.response.DoubtData;
+import com.pw.nativeplayer.response.FAQData;
+import com.pw.nativeplayer.response.RecentActivityData;
+import com.pw.nativeplayer.response.Schedules;
+import com.pw.nativeplayer.response.Solution;
+import com.pw.nativeplayer.response.StoreListData;
+import com.pw.nativeplayer.response.SubjectListData;
+import com.pw.nativeplayer.response.TodayClassesData;
+import com.pw.nativeplayer.response.TopicContentData;
+import com.pw.nativeplayer.response.VideoBookmarkData;
+import com.pw.nativeplayer.response.VideoDetails;
 import com.pw.nativeplayer.utils.NativePlayer;
 import com.pw.nativeplayer.utils.NetworkManager;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+
+import deva.youtube.generator.youtubequalitygenerator.R;
 
 @NativePlugin
 public class DevaYoutubeGenerator extends Plugin {
@@ -29,12 +46,103 @@ public class DevaYoutubeGenerator extends Plugin {
         String token = call.getString("token");
         String responseHeaders = call.getString("responseHeaders");
         String extraData = call.getString("extraData");
+        String responseMapper = call.getString("responseMapper");
 
         NetworkManager networkManager = new NetworkManager(getContext());
         networkManager.getLoginManager().setAPIToken(token);
-        Object response = videoData;
+
         NativePlayerModal nativePlayerModal = new Gson().fromJson(extraData, NativePlayerModal.class);
-        NativePlayer.playVideo(getContex(),response,nativePlayerModal);
+
+        switch (responseMapper){
+            case ResponseMapper.VIDEO_DETAILS : {
+                VideoDetails videoDetails = new Gson().fromJson(videoData, VideoDetails.class);
+                NativePlayer.playVideo(getContext(),videoDetails,nativePlayerModal);
+                break;
+            }
+
+            case ResponseMapper.COURSE_CONTENT_DATA: {
+                CourseContentData courseContentData = new Gson().fromJson(videoData, CourseContentData.class);
+                NativePlayer.playVideo(getContext(),courseContentData,nativePlayerModal);
+                break;
+            }
+
+            case ResponseMapper.TOPIC_CONTENT_DATA: {
+                TopicContentData topicContentData = new Gson().fromJson(videoData, TopicContentData.class);
+                NativePlayer.playVideo(getContext(),topicContentData,nativePlayerModal);
+                break;
+            }
+
+            case ResponseMapper.RECENT_ACTIVITY_DATA: {
+                RecentActivityData recentActivityData = new Gson().fromJson(videoData, RecentActivityData.class);
+                NativePlayer.playVideo(getContext(),recentActivityData,nativePlayerModal);
+                break;
+            }
+
+            case ResponseMapper.VIDEO_BOOKMARK_DATA: {
+                VideoBookmarkData videoBookmarkData = new Gson().fromJson(videoData, VideoBookmarkData.class);
+                NativePlayer.playVideo(getContext(),videoBookmarkData,nativePlayerModal);
+                break;
+            }
+
+            case ResponseMapper.SUBJECT_LIST_DATA: {
+                SubjectListData subjectListData = new Gson().fromJson(videoData, SubjectListData.class);
+                NativePlayer.playVideo(getContext(),subjectListData,nativePlayerModal);
+                break;
+            }
+
+            case ResponseMapper.DOUBT_DATA: {
+                DoubtData doubtData = new Gson().fromJson(videoData, DoubtData.class);
+                NativePlayer.playVideo(getContext(),doubtData,nativePlayerModal);
+                break;
+            }
+
+            case ResponseMapper.BATCH_DATA: {
+                BatchData batchData = new Gson().fromJson(videoData, BatchData.class);
+                NativePlayer.playVideo(getContext(),batchData,nativePlayerModal);
+                break;
+            }
+
+            case ResponseMapper.SCHEDULES: {
+                Schedules schedules = new Gson().fromJson(videoData, Schedules.class);
+                NativePlayer.playVideo(getContext(),schedules,nativePlayerModal);
+                break;
+            }
+
+            case ResponseMapper.TODAY_CLASSES_DATA: {
+                TodayClassesData todayClassesData = new Gson().fromJson(videoData, TodayClassesData.class);
+                NativePlayer.playVideo(getContext(),todayClassesData,nativePlayerModal);
+                break;
+            }
+
+            case ResponseMapper.DEMO_VIDEOS_DATA: {
+                DemoVideosData demoVideosData = new Gson().fromJson(videoData, DemoVideosData.class);
+                NativePlayer.playVideo(getContext(),demoVideosData,nativePlayerModal);
+                break;
+            }
+
+            case ResponseMapper.STORE_LIST_DATA: {
+                StoreListData storeListData = new Gson().fromJson(videoData, StoreListData.class);
+                NativePlayer.playVideo(getContext(),storeListData,nativePlayerModal);
+                break;
+            }
+
+            case ResponseMapper.SOLUTION: {
+                Solution solution = new Gson().fromJson(videoData, Solution.class);
+                NativePlayer.playVideo(getContext(),solution,nativePlayerModal);
+                break;
+            }
+
+            case ResponseMapper.FAQ_DATA: {
+                FAQData faqData = new Gson().fromJson(videoData, FAQData.class);
+                NativePlayer.playVideo(getContext(),faqData,nativePlayerModal);
+                break;
+            }
+
+            default: {
+                Toast.makeText(getContext(), getContext().getString(R.string.response_not_handled),Toast.LENGTH_SHORT).show();
+            }
+
+        }
 
     }
 
